1a4a003d39a9da13e4e43cfa46384ce90e4dee1a
Manuel Rodriguez <mrodriguezrodriguez@cifpfbmoll.eu>
2024-06-30 18:32:06 +0200

    learning how signals work

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[0]);
 
+  // Así se haría con signals.
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.selectedUser().avatar;
+  });
+
+  // Manera tradicional
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 }
