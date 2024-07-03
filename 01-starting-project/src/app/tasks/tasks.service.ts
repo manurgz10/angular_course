68fe67041b318c40333cfdb76b1cb108bfb847bf
Manuel Rodriguez <mrodriguezrodriguez@cifpfbmoll.eu>
2024-07-03 13:02:18 +0200

    Using localStorage to save tasks data

--- 01-starting-project/src/app/tasks/tasks.service.ts
+++ 01-starting-project/src/app/tasks/tasks.service.ts
@@ -1,50 +1,63 @@
-import { Injectable } from "@angular/core";
-import { NewTaskData } from "../task/task.model";
+import { Injectable } from '@angular/core';
+import { NewTaskData } from '../task/task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-    private tasks = [
-        {
-          id: 't1',
-          userId: 'u1',
-          title: 'Master Angular',
-          summary:
-            'Learn all the basic and advanced features of Angular & how to apply them.',
-          dueDate: '2025-12-31',
-        },
-        {
-          id: 't2',
-          userId: 'u3',
-          title: 'Build first prototype',
-          summary: 'Build a first prototype of the online shop website',
-          dueDate: '2024-05-31',
-        },
-        {
-          id: 't3',
-          userId: 'u3',
-          title: 'Prepare issue template',
-          summary:
-            'Prepare and describe an issue template which will help with project management',
-          dueDate: '2024-06-15',
-        },
-      ];
-    
-      getUserTasks(selectedId: string) {
-        return this.tasks.filter((task) => task.userId === selectedId);
-      }
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
 
-      addTask(task: NewTaskData, selectedId: string) {
-        this.tasks.push({
-          id: Math.random().toString(),
-          userId: selectedId,
-          title: task.title,
-          summary: task.summary,
-          dueDate: task.date,
-        });
-      }
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
 
-        completeTask(id: string) {
-            this.tasks = this.tasks.filter((task) => task.id !== id);
-        }
- }
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
+  getUserTasks(selectedId: string) {
+    return this.tasks.filter((task) => task.userId === selectedId);
+  }
+
+  addTask(task: NewTaskData, selectedId: string) {
+    this.tasks.push({
+      id: Math.random().toString(),
+      userId: selectedId,
+      title: task.title,
+      summary: task.summary,
+      dueDate: task.date,
+    });
+    this.saveTasks();
+  }
+
+  completeTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+}
